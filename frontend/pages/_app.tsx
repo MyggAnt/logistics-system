687fd160adec5412dfc3bb7e252933c1979d2f6d
MyggAnt <yumgkt@gmail.com>
2025-08-29 23:44:02 +0400

    feat: complete full-stack setup with Apollo Client, GraphQL integration, and working frontend-backend connection

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,13 +3,17 @@ import 'antd/dist/reset.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '../context/ThemeContext';
 import { LayoutEventsProvider } from '../context/LayoutEvents';
+import { ApolloProvider } from '@apollo/client';
+import client from '../lib/apollo-client';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <LayoutEventsProvider>
-        <Component {...pageProps} />
-      </LayoutEventsProvider>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider>
+        <LayoutEventsProvider>
+          <Component {...pageProps} />
+        </LayoutEventsProvider>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
